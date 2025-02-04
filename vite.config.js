d0249cefa2eb4d4e793e128ab5fcd0c1d15cce08
Danny Valdivia <dluis.valdivia@gmail.com>
2025-02-04 16:02:46 +0000

    vitecongig update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/SarahKB',
-  plugins: [react()],
+  base: '/',
+  plugins: [
+    React(),
+    {
+      name: 'add-cname',
+      closeBundle() {
+        const cnamePath = path.resolve('dist/assets/CNAME')
+        writeFileSync(cnamePath, 'thatsveryadhd.com')
+      },
+    },
+  ],
 })
